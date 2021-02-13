3372a8a6b029896edd34050a6a25e359400d018c
Yiğit Tanrıverdi <x_game92@hotmail.com>
2021-02-13 22:17:07 +0100

    remove unused end point

--- server/AuthServer/AuthServer/Controllers/AuthController.cs
+++ server/AuthServer/AuthServer/Controllers/AuthController.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace AuthServer.Controllers
 {
-    
+
     [ApiController]
     [Route("api/v1/auth")]
     public class AuthController : Controller
@@ -19,21 +19,21 @@ namespace AuthServer.Controllers
         {
             _authService = authService;
         }
-        
+
         [HttpPost("login")]
         public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
         {
             ThreadSleepRandomly();
-            
+
             var userToLogin = _authService.Login(userForLoginDto);
-            
+
             if (!userToLogin.Success)
             {
                 return BadRequest(new ErrorResponseDto(userToLogin.Message));
             }
 
             var result = _authService.CreateAccessToken(userToLogin.Data);
-            
+
             if (result.Success)
             {
                 return Ok(result.Data);
@@ -41,27 +41,20 @@ namespace AuthServer.Controllers
 
             return BadRequest(new ErrorResponseDto(userToLogin.Message));
         }
-        
+
         [HttpPost("logout")]
         public async Task<IActionResult> CancelAccessToken()
         {
             ThreadSleepRandomly();
             await _authService.DeactivateCurrentAsync();
- 
+
             return NoContent();
-        }
-        
-        [Authorize]
-        [HttpGet("validate")]
-        public async Task<IActionResult> Test()
-        {
-            return Ok();
         }
 
         private void ThreadSleepRandomly()
-        { 
+        {
             Random random = new Random();
-            var mseconds=random.Next(1, 3) * 1000;   
+            var mseconds=random.Next(1, 3) * 1000;
             Thread.Sleep(mseconds);
         }
     }
